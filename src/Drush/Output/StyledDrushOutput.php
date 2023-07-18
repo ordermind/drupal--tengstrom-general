b2fbb9b57f840a08d8831599e44fea72259664a6
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-07-18 22:17:07 +0200

    Add a service for a styled drush output object

---
+++ src/Drush/Output/StyledDrushOutput.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\tengstrom_general\Drush\Output;
+
+use Drush\Drush;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StyledDrushOutput {
+
+  public function success(string $message): void {
+    $this->getInstance()->writeln('<success>[success]</success> ' . $message);
+  }
+
+  public function notice(string $message): void {
+    $this->getInstance()->writeln('<notice>[notice]</notice> ' . $message);
+  }
+
+  public function error(string $message): void {
+    $this->getInstance()->writeln('<error>[error]</error> <error>' . $message . '</error>');
+  }
+
+  public function blankLine(): void {
+    $this->getInstance()->writeln('');
+  }
+
+  /**
+   * This is a workaround for the fact that Drush uses a separate DI container,
+   * which means that we can't inject the drush output class as a service
+   * because then it will throw an error when using the web interface because
+   * the service only exists in the Drush container, not in the regular Drupal
+   * container.
+   */
+  public function getInstance(): OutputInterface {
+    $initializeNewInstance = function () {
+      $output = Drush::output();
+      $formatter = $output->getFormatter();
+
+      $successStyle = new OutputFormatterStyle('white', 'green', ['font-weight' => 'bold']);
+      $formatter->setStyle('success', $successStyle);
+
+      $noticeStyle = new OutputFormatterStyle('white', 'cyan', ['font-weight' => 'bold']);
+      $formatter->setStyle('notice', $noticeStyle);
+
+      $errorStyle = new OutputFormatterStyle('white', 'red', ['font-weight' => 'bold']);
+      $formatter->setStyle('error', $errorStyle);
+
+      return $output;
+    };
+
+    $output = &drupal_static(static::class . '::getReference');
+    if (!$output) {
+      $output = $initializeNewInstance();
+    }
+
+    return $output;
+  }
+
+}
