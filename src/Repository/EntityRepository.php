858ea316162f3fb2576ee059790703c3d298f43b
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-08-01 12:07:28 +0200

    Add custom entity repository

---
+++ src/Repository/EntityRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\tengstrom_general\Repository;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+class EntityRepository {
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  public function countEntitiesOfType(string $entityTypeId): int {
+    $storage = $this->entityTypeManager->getStorage($entityTypeId);
+
+    return $storage->getQuery()->accessCheck(FALSE)->count()->execute();
+  }
+
+  /**
+   * @return \Generator|int[][]
+   */
+  public function fetchEntityIdsOfType(string $entityTypeId, int $chunkSize): \Generator {
+    $storage = $this->entityTypeManager->getStorage($entityTypeId);
+
+    $entityQuery = $storage->getQuery()
+      ->accessCheck(FALSE);
+
+    while (
+      $entityIds = $entityQuery
+        ->range(0, $chunkSize)
+        ->execute()
+    ) {
+      yield $entityIds;
+    }
+  }
+
+  /**
+   * @return \Generator|EntityInterface[][]
+   */
+  public function fetchEntitiesOfType(string $entityTypeId, int $chunkSize): \Generator {
+    $storage = $this->entityTypeManager->getStorage($entityTypeId);
+
+    foreach ($this->fetchEntityIdsOfType($entityTypeId, $chunkSize) as $entityIds) {
+      yield $storage->loadMultiple($entityIds);
+    }
+  }
+
+}
