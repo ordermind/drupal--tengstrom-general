26a173bdc98c49b8ed8c6ced0e2856fe81115ed2
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-08-04 14:52:16 +0200

    Refactor TestEntityRepository

--- tests/src/Unit/Fixtures/TestEntityRepository.php
+++ tests/src/Unit/Fixtures/TestEntityRepository.php
@@ -5,47 +5,53 @@ declare(strict_types=1);
 namespace Drupal\Tests\tengstrom_general\Unit\Fixtures;
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\tengstrom_general\Repository\EntityRepositoryInterface;
+use Ordermind\DrupalTengstromShared\Test\Fixtures\EntityStorage\ConfigEntityArrayStorage;
 use Ordermind\DrupalTengstromShared\Test\Fixtures\EntityStorage\ContentEntityArrayStorage;
 
-class TestContentEntityRepository implements EntityRepositoryInterface {
-  protected ContentEntityArrayStorage $storage;
+class TestEntityRepository implements EntityRepositoryInterface {
+  protected EntityTypeManagerInterface $entityTypeManager;
 
-  public function __construct(ContentEntityArrayStorage $storage) {
-    $this->storage = $storage;
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
-   * @param int $entityId
+   * @param string $entityId
    */
   public function hasEntityIdForType(string $entityTypeId, int|string $entityId): bool {
-    return $this->storage->hasEntityId($entityId);
+    return $this->getStorage($entityTypeId)->hasEntityId($entityId);
   }
 
   public function countEntitiesOfType(string $entityTypeId): int {
-    return $this->storage->count();
+    return $this->getStorage($entityTypeId)->count();
   }
 
   /**
-   * @return int[]
+   * @return string[]
    */
   public function fetchEntityIdsOfType(string $entityTypeId, int $offset = 0, int $chunkSize = 100): array {
     return array_map(
-      fn (EntityInterface $entity) => (int) $entity->id(),
+      fn (EntityInterface $entity) => (string) $entity->id(),
       $this->fetchEntitiesOfType($entityTypeId, $offset, $chunkSize)
     );
   }
 
   public function fetchEntitiesOfType(string $entityTypeId, int $offset = 0, int $chunkSize = 100): array {
-    $items = $this->storage->loadAll();
+    $items = $this->getStorage($entityTypeId)->loadAll();
 
     return array_slice($items, $offset, $chunkSize);
   }
 
   public function fetchFirstEntityOfType(string $entityTypeId): ?EntityInterface {
-    $items = $this->storage->loadAll();
+    $items = $this->getStorage($entityTypeId)->loadAll();
 
     return reset($items);
+  }
+
+  protected function getStorage(string $entityTypeId): ContentEntityArrayStorage | ConfigEntityArrayStorage {
+    return $this->entityTypeManager->getStorage($entityTypeId);
   }
 
 }
